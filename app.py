b3d068b9dc7ee20e9c06264e52958128849ace34
Sabdrine200 <davidfanou200@gmail.com>
2025-06-24 00:24:02 +0100

    Update app.py
    Signed-off-by: Sabdrine200 <davidfanou200@gmail.com>

--- app.py
+++ app.py
@@ -9,19 +9,19 @@ def accueil():
 
 @app.route('/conseils')
 def conseils():
-    with open("data/conseils.json", "r") as f:
+    with open("static/data/conseils.json", "r", encoding="utf-8") as f:
         data = json.load(f)
     return render_template("conseils.html", conseils=data)
 
 @app.route('/anomalies')
 def anomalies():
-    with open("data/anomalies.json", "r") as f:
+    with open("static/data/anomalies.json", "r", encoding="utf-8") as f:
         data = json.load(f)
     return render_template("anomalies.html", anomalies=data)
 
 @app.route('/scores')
 def scores():
-    with open("data/scores.json", "r") as f:
+    with open("static/data/scores.json", "r", encoding="utf-8") as f:
         data = json.load(f)
     return render_template("scores.html", scores=data)
 
