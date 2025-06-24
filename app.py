d0836beef8eb5a3f69263b31b9ae3496deb2b3b3
Sabdrine200 <davidfanou200@gmail.com>
2025-06-24 01:22:20 +0100

    Update app.py
    Signed-off-by: Sabdrine200 <davidfanou200@gmail.com>

--- app.py
+++ app.py
@@ -25,5 +25,8 @@ def scores():
         data = json.load(f)
     return render_template("scores.html", scores=data)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render définit le port ici
+    app.run(host="0.0.0.0", port=port)
